02a37db0b6213aad4c9585da5450e42bc4fd296a
shui-dun <2604697844@qq.com>
2023-05-08 10:42:40 +0800

    完全注册和登录页面

--- doc/To-do List.md
+++ doc/To-do List.md
@@ -1,6 +1,6 @@
 # To-do List
 
-- [ ] 学习vue https://developer.mozilla.org/zh-CN/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Vue_first_component
+- [x] 学习vue
 - [ ] 学习Phaser.js，跑起简单的样例
 
 
--- sandbox_town_frontend/src/App.vue
+++ sandbox_town_frontend/src/App.vue
@@ -1,21 +1,15 @@
 <template>
-  <h1>To-Do List</h1>
+  <div id="app">
+    <AuthForm />
+  </div>
 </template>
 
 <script>
+import AuthForm from './views/AuthForm.vue';
 
 export default {
-  name: 'App'
-}
+  components: {
+    AuthForm,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
