5f60ddc3daec2b6b014b9c337ab9922ae1ec71b7
shui-dun <2604697844@qq.com>
2023-05-09 20:51:30 +0800

    完成背包按钮

---
+++ sandbox_town_frontend/src/components/FloatingButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="floating-button">
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+    },
+};
+</script>
+<style scoped>
+.floating-button {
+    background-image: url('../assets/img/backpack.jpg');
+    background-size: 100% 100%;
+    /* 固定位置 */
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    width: 50px;
+    /* 可根据需要调整大小 */
+    height: 50px;
+    border-radius: 50%;
+    /* border-width: 50px;
+    border-color: #25b0f3; */
+    /* 可根据需要调整背景颜色 */
+    /* display: flex; */
+    /* justify-content: center; */
+    /* align-items: center; */
+    cursor: pointer;
+    z-index: 9999;
+    /* 确保按钮位于页面的最上层 */
+}
+</style>
+  
--- sandbox_town_frontend/src/views/GameView.vue
+++ sandbox_town_frontend/src/views/GameView.vue
@@ -1,23 +1,32 @@
 <template>
     <game-canvas @itemClicked="bar"></game-canvas>
+    <FloatingButton @click="clickBackpack" />
+    <div v-if="backpackOpened"></div>
 </template>
 
 <script>
 import GameCanvas from '@/components/GameCanvas.vue';
+import FloatingButton from '@/components/FloatingButton.vue';
+
 
 export default {
     components: {
         GameCanvas,
+        FloatingButton,
     },
     props: {
     },
     data() {
         return {
+            backpackOpened: false,
         };
     },
     methods: {
         bar() {
             alert("itemClicked!");
+        },
+        clickBackpack() {
+            this.backpackOpened = true;
         }
     },
     computed: {
