9725cef7ead2ca15aa220333159baad6cbf59848
shui-dun <2604697844@qq.com>
2023-05-23 18:45:42 +0800

    修复shiro导致很多spring注解坏掉的bug，解决方法是禁用shiro的注解

--- sandbox_town_backend/src/main/java/com/shuidun/sandbox_town_backend/config/ShiroConfig.java
+++ sandbox_town_backend/src/main/java/com/shuidun/sandbox_town_backend/config/ShiroConfig.java
@@ -2,38 +2,19 @@ package com.shuidun.sandbox_town_backend.config;
 
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.crypto.hash.Sha256Hash;
-import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.mgt.SessionsSecurityManager;
-import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 @Configuration
-@EnableCaching
 public class ShiroConfig {
     @Autowired
     private CustomRealm customRealm;
-
-    /**
-     * 配置shiro的过滤器工厂（哪些路径需要哪些权限才能访问）
-     */
-    @Bean
-    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager securityManager) {
-        ShiroFilterFactoryBean filterFactoryBean = new ShiroFilterFactoryBean();
-        // 关联SecurityManager
-        filterFactoryBean.setSecurityManager(securityManager);
-        // 添加shiro过滤器
-        Map<String, String> map = new LinkedHashMap<>();
-        map.put("/**", "anon");
-        filterFactoryBean.setFilterChainDefinitionMap(map);
-        return filterFactoryBean;
-    }
 
     /**
      * 配置SecurityManager，与安全有关的操作都会与SecurityManager进行交互；管理着所有的Subject；它是Shiro的核心，负责与Shiro的其他组件进行交互
@@ -41,7 +22,7 @@ public class ShiroConfig {
     @Bean
     public DefaultWebSecurityManager getDefaultWebSecurityManager() {
         DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
-        //设置登录登录认证开启salt加密
+        // 设置登录登录认证开启salt加密
         HashedCredentialsMatcher hashMatcher = new HashedCredentialsMatcher();
         hashMatcher.setHashAlgorithmName(Sha256Hash.ALGORITHM_NAME);
         hashMatcher.setStoredCredentialsHexEncoded(false);
@@ -49,6 +30,36 @@ public class ShiroConfig {
         customRealm.setCredentialsMatcher(hashMatcher);
         manager.setRealm(customRealm);
         return manager;
+    }
+
+    /** 配置过滤器，哪些路径需要哪些权限才能访问 */
+    @Bean
+    public ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition chainDefinition = new DefaultShiroFilterChainDefinition();
+        chainDefinition.addPathDefinition("/user/foo", "authc");
+        chainDefinition.addPathDefinition("/**", "anon");
+        return chainDefinition;
+    }
+
+
+    /**
+     * 遇到一个特别恶心🤮的事情，shiro导致spring的一些注解失效（例如@Cacheable），弄了一天才找到解决办法：
+     * 禁用shiro的注解，这样就不会和spring的注解冲突
+     * 禁用shiro的注解通过在配置类中添加如下两个bean DisableShiroAnnotationFilter和DefaultAdvisorAutoProxyCreator实现
+     */
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        defaultAdvisorAutoProxyCreator.setProxyTargetClass(true);
+        defaultAdvisorAutoProxyCreator.setUsePrefix(false);
+        return defaultAdvisorAutoProxyCreator;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(DefaultWebSecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        return authorizationAttributeSourceAdvisor;
     }
 
 }
