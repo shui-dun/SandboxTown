e3eab02b2d64b866aaa127fda65d5da8010d11bc
shui-dun <2604697844@qq.com>
2023-06-11 14:45:28 +0800

    日志中记录用户名

--- sandbox_town_backend/src/main/java/com/shuidun/sandbox_town_backend/exception/GlobalExceptionHandler.java
+++ sandbox_town_backend/src/main/java/com/shuidun/sandbox_town_backend/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.shuidun.sandbox_town_backend.exception;
 
 import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.stp.StpUtil;
 import com.shuidun.sandbox_town_backend.bean.RestResponse;
 import com.shuidun.sandbox_town_backend.enumeration.StatusCodeEnum;
 import lombok.extern.slf4j.Slf4j;
@@ -13,31 +14,39 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static String currentUser() {
+        if (!StpUtil.isLogin()) {
+            return null;
+        } else {
+            return StpUtil.getLoginIdAsString();
+        }
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public RestResponse<?> exceptionHandler(Exception e) {
-        log.error("e=\"{}\". Stack is :\n", e.getMessage(), e);
+        log.error("User: {} Stack is :\n", currentUser(), e);
         return new RestResponse<>(StatusCodeEnum.SERVER_ERROR);
     }
 
     @ExceptionHandler(NotLoginException.class)
     @ResponseBody
     public RestResponse<?> exceptionHandler(NotLoginException e) {
-        log.error("e=\"{}\". Stack is :\n", e.getMessage(), e);
+        log.error("e: \"{}\". User: {}\n", e.getMessage(), currentUser());
         return new RestResponse<>(StatusCodeEnum.NO_PERMISSION);
     }
 
     @ExceptionHandler(BusinessException.class)
     @ResponseBody
     public RestResponse<?> exceptionHandler(BusinessException e) {
-        log.error("e=\"{}\". Stack is :\n", e.getMessage(), e);
+        log.error("e: \"{}\". User: {}\n", e.getMessage(), currentUser());
         return new RestResponse<>(e.getStatusCode());
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     @ResponseBody
     public RestResponse<?> exceptionHandler(HttpRequestMethodNotSupportedException e) {
-        log.error("e=\"{}\". Stack is :\n", e.getMessage(), e);
+        log.error("e: \"{}\". User: {}\n", e.getMessage(), currentUser());
         return new RestResponse<>(StatusCodeEnum.REQUEST_METHOD_NOT_SUPPORTED);
     }
 
